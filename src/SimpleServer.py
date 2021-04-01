d6637b9bce03c3ddd3d159748bdc87c102407c26
faithcoslett <faithkcoslett@gmail.com>
2021-04-01 19:26:29 +0000

    Testing how to save my changes to GitHub without starting a pull request

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -59,29 +59,53 @@ def surnameSearch():
 			#return str(data)
 			return render_template('Employee.html', data = data)
 
+@app.route("/Employee/UpdateEmployee", methods = ['POST','GET'])
+def studentAddDetails():
+	if request.method =='GET':
+		return render_template('EmployeeData.html')
+	if request.method =='POST':
+		firstName = request.form.get('firstName', default="Error") 
+		lastName = request.form.get('lastName', default="Error")
+		state = request.form.get('state', default="Error")
+		print("updating employee"+firstName)
+		try:
+			conn = sqlite3.connect(DATABASE)
+			cur = conn.cursor()
+			cur.execute("UPDATE EmployeeList SET 'State/Province' = state WHERE 'FirstName' = firstName\
+						AND 'LastName' = lastName")
+
+			conn.commit()
+			msg = "Record successfully updated"
+		except:
+			conn.rollback()
+			msg = "error in update operation"
+		finally:
+			conn.close()
+			return msg
+
 
 
 # The name says it...
-@app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
-def surnameInjectionSearch():
-	if request.method =='GET':
-		return render_template('EmployeeSQLInjection.html')
-	if request.method =='POST':
-		lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
-		conn = sqlite3.connect(DATABASE)
-		cur = conn.cursor()
+#@app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
+#def surnameInjectionSearch():
+#	if request.method =='GET':
+#		return render_template('EmployeeSQLInjection.html')
+#	if request.method =='POST':
+#		lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
+#		conn = sqlite3.connect(DATABASE)
+#		cur = conn.cursor()
 		
 		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-		query = "SELECT * FROM EmployeeList WHERE lastname= '%s' " % (lastName,)
-		print (query)
-		cur.execute(query)
+#		query = "SELECT * FROM EmployeeList WHERE lastname= '%s' " % (lastName,)
+#		print (query)
+#		cur.execute(query)
 		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
 		
-		data = cur.fetchall()
-		print (data)
-		print (lastName)
-		conn.close()
-		return render_template('Employee.html', data = data)
+#		data = cur.fetchall()
+#		print (data)
+#		print (lastName)
+#		conn.close()
+#		return render_template('Employee.html', data = data)
 
 
 if __name__ == "__main__":
