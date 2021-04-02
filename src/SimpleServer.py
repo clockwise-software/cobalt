8e35f8b6af91085c18562953eaf47a186a560be9
faithcoslett <faithkcoslett@gmail.com>
2021-04-02 00:17:06 +0000

    made duplicate employee table with id key

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -1,6 +1,6 @@
-## CLOCKWISE-BOOTCAMP SimpleServer.py 
-## Based on Server from Dr. Ian Cooper @ Cardiff
-## Updated by Dr. Mike Borowczak @ UWyo March 2021
+# CLOCKWISE-BOOTCAMP SimpleServer.py
+# Based on Server from Dr. Ian Cooper @ Cardiff
+# Updated by Dr. Mike Borowczak @ UWyo March 2021
 
 import os
 from flask import Flask, redirect, request, render_template
@@ -10,104 +10,125 @@ DATABASE = 'bootcamp.db'
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def basic():
     return render_template('Employee.html')
 
-@app.route("/Employee/AddEmployee", methods = ['POST','GET'])
+
+@app.route("/Employee/AddEmployee", methods=['POST', 'GET'])
 def studentAddDetails():
-	if request.method =='GET':
-		return render_template('EmployeeData.html')
-	if request.method =='POST':
-		firstName = request.form.get('firstName', default="Error") 
-		lastName = request.form.get('lastName', default="Error")
-		businessunit = request.form.get('bu', default="Error")
-		state = request.form.get('state', default="Error")
-		print("inserting employee"+firstName)
-		try:
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("INSERT INTO EmployeeList ('FirstName', 'LastName', 'Business Unit', 'State/Province')\
-						VALUES (?,?,?,?)",(firstName, lastName, businessunit, state ) )
+    if request.method == 'GET':
+        return render_template('EmployeeData.html')
+    if request.method == 'POST':
+        firstName = request.form.get('firstName', default="Error")
+        lastName = request.form.get('lastName', default="Error")
+        businessunit = request.form.get('bu', default="Error")
+        state = request.form.get('state', default="Error")
+        print("inserting employee"+firstName)
+        try:
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            cur.execute("INSERT INTO EmployeeList ('FirstName', 'LastName', 'Business Unit', 'State/Province')\
+						VALUES (?,?,?,?)", (firstName, lastName, businessunit, state))
 
-			conn.commit()
-			msg = "Record successfully added"
-		except:
-			conn.rollback()
-			msg = "error in insert operation"
-		finally:
-			conn.close()
-			return msg
+            conn.commit()
+            msg = "Record successfully added"
+        except:
+            conn.rollback()
+            msg = "error in insert operation"
+        finally:
+            conn.close()
+            return msg
 
-@app.route("/Employee/Search", methods = ['GET','POST'])
+
+@app.route("/Employee/Search", methods=['GET', 'POST'])
 def surnameSearch():
-	if request.method =='GET':
-		return render_template('EmployeeSearch.html')
-	if request.method =='POST':
-		try:
-			lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("SELECT * FROM 'EmployeeList' WHERE LastName=?", [lastName])
-			data = cur.fetchall()
-			print(data)
-		except:
-			print('there was an error', data)
-			conn.close()
-		finally:
-			conn.close()
-			#return str(data)
-			return render_template('Employee.html', data = data)
+    if request.method == 'GET':
+        return render_template('EmployeeSearch.html')
+    if request.method == 'POST':
+        try:
+            # rem: args for get form for post
+            lastName = request.form.get('lastName', default="Error")
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            cur.execute(
+                "SELECT * FROM 'EmployeeList' WHERE LastName=?", [lastName])
+            data = cur.fetchall()
+            print(data)
+        except:
+            print('there was an error', data)
+            conn.close()
+        finally:
+            conn.close()
+            # return str(data)
+            return render_template('Employee.html', data=data)
 
-@app.route("/Employee/UpdateEmployee", methods = ['POST','GET'])
+
+@app.route("/Employee/UpdateEmployee", methods=['POST', 'GET'])
 def studentUpdateDetails():
-	if request.method =='GET':
-		return render_template('EmployeeUpdate.html')
-	if request.method =='POST':
-		firstName = request.form.get('firstName', default="Error") 
-		lastName = request.form.get('lastName', default="Error")
-		state = request.form.get('state', default="Error")
-		print("updating employee"+firstName)
-		try:
-			conn = sqlite3.connect(DATABASE)
-			cur = conn.cursor()
-			cur.execute("UPDATE 'EmployeeList' SET 'State/Province'=? WHERE FirstName=?\
+    if request.method == 'GET':
+        xid = request.args.get('xid', default="Error")
+        print("label"+xid)
+        try:
+            # rem: args for get form for post
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            cur.execute("SELECT * FROM 'EmployeeList' WHERE Id=?", [xid])
+            data = cur.fetchall()
+            print(data)
+        except:
+            print('there was an error', data)
+            conn.close()
+        finally:
+            conn.close()
+            #if data = NULL, initialize it (array with as many empty strings in it as there are columns to fill out for employee ###########
+            employee = data[0]
+            # return str(data)
+            return render_template('EmployeeUpdate.html',data=employee)
+    if request.method == 'POST':
+        firstName = request.form.get('firstName', default="Error")
+        lastName = request.form.get('lastName', default="Error")
+        state = request.form.get('state', default="Error")
+        print("updating employee"+firstName)
+        try:
+            conn = sqlite3.connect(DATABASE)
+            cur = conn.cursor()
+            cur.execute("UPDATE 'EmployeeList' SET 'State/Province'=? WHERE FirstName=?\
 						AND LastName=?", (state, firstName, lastName))
 
-			conn.commit()
-			msg = "Record successfully updated"
-		except:
-			conn.rollback()
-			msg = "error in update operation"
-		finally:
-			conn.close()
-			return msg
-
+            conn.commit()
+            msg = "Record successfully updated"
+        except:
+            conn.rollback()
+            msg = "error in update operation"
+        finally:
+            conn.close()
+            return msg
 
 
 # The name says it...
-#@app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
-#def surnameInjectionSearch():
+# @app.route("/Employee/VulnerableSearch", methods = ['GET','POST'])
+# def surnameInjectionSearch():
 #	if request.method =='GET':
 #		return render_template('EmployeeSQLInjection.html')
 #	if request.method =='POST':
 #		lastName = request.form.get('lastName', default="Error") #rem: args for get form for post
 #		conn = sqlite3.connect(DATABASE)
 #		cur = conn.cursor()
-		
-		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
+
+        # VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
 #		query = "SELECT * FROM EmployeeList WHERE lastname= '%s' " % (lastName,)
 #		print (query)
 #		cur.execute(query)
-		# VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-		
+        # VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
+
 #		data = cur.fetchall()
 #		print (data)
 #		print (lastName)
 #		conn.close()
 #		return render_template('Employee.html', data = data)
 
-
 if __name__ == "__main__":
-	app.run(debug=True)
-	app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
