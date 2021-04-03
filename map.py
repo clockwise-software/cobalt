de13cbac0a8ff8d3761f64b1493064740242e946
ZiaMacdermid <ziajanemac@gmail.com>
2021-04-03 22:30:46 +0000

    h

--- map.py
+++ map.py
@@ -11,11 +11,11 @@ states = states.to_crs("EPSG:3395")
 
 cities = pd.read_csv('/workspace/cobalt/UScities.csv', header = 0, index_col= 0,names=['city', 'lat', 'lng', 'stateId', 'stateName', 'countyName', 'countyFips'])
 
-states.plot()
-plt.show()
-plt.axis([-2e7,-0.5e7,0,1.2e7])
-latitude = [(1e6)*i for i in cities['lat']]
-longitude = [(1e6)*i for i in cities['lng']]
+#states.plot()
+#plt.show()
+##plt.axis([-2e7,-0.5e7,0,1.2e7])
+latitude = cities['lat']
+longitude = cities['lng']
 plt.plot(latitude, longitude, 'r+')
 
 #states[states['NAME'] == 'Texas'].plot(figsize=(12, 12))
